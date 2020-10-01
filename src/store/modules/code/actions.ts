f8125aa27ddfb3e691fb54791354d1456530e841
조성현 <>
2020-10-01 20:16:06 +0900

    console.log 허용

--- src/store/modules/code/actions.ts
+++ src/store/modules/code/actions.ts
@@ -5,7 +5,7 @@ import { State } from './state';
 const action: ActionTree<State, any> = {
   async CODE_LIST(store: any, payload: any): Promise<void> {
     const response = await contactAPI.CODE_LIST();
-    //console.log(response);
+    console.log(response);
     await store.commit('CODE_LIST', { resultsList: response.resultsList, totalcount: response.totalCount });
   },
 };
